fe7d59e42223bab584918e18e395e9dbd03277e4
Adrian Furmanek <adrian.furm@gmail.com>
2022-02-25 14:06:42 +0100

    random quotes from API and local

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('test');
+let apiQuotes =[];
+
+// Random Quote
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Local Quote
+// function localNewQuote() {
+//     const localQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(localQuote);
+// }
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Error here
+    }
+}
+
+// On Load
+getQuotes();
+// localNewQuote();
