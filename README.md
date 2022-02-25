25a3a3071e6e5d2d64701c8b1d9545537ab5b0e7
Adrian Furmanek <adrian.furm@gmail.com>
2022-02-25 16:38:47 +0100

    Loader

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dQuote
-Daily quotes for you!
+dQuote - the app generates the best quotes for you to improve your day.
+You can tweet your quote immediately.
+So take your quote for today and have a nice day! :)
--- script.js
+++ script.js
@@ -1,28 +1,71 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes =[];
+
+function showLoading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoading() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // Random Quote
 function newQuote() {
+    showLoading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author is null
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check long quotes
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    hideLoading();
 }
 
 // Local Quote
 // function localNewQuote() {
 //     const localQuote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-//     console.log(localQuote);
+//     authorText.textContent = quote.author;
+//     quoteText.textContent = quote.text;
 // }
 
 // Get Quotes from API
 async function getQuotes() {
+    showLoading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-        // Error here
+        alert(`Error: ${error}`);
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listneres
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
